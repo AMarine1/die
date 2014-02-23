d024daa4d3508450123be997d2d5f2f69f6e4147
Jeremy Brown <Jeremy.Ryan.Brown@gmail.com>
2014-02-23 14:43:35 -0800

    uploding files

---
+++ scripts/functions.js
@@ -0,0 +1,12 @@
+function $(e) {
+	return document.querySelector(e);
+}
+window.addEventListener('load', function(){
+	$('output[for=test]').value = $('input#test').value;
+	$('input#test').addEventListener('input',function(){
+		$('output[for=' + this.id + ']').value = this.value;
+	});
+	$('button').addEventListener('click',function(){
+		alert('Hello, ' + $('#name').value)
+	});
+});
